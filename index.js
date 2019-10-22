e9ce36dc962f9f180358a489a1cfaa7d9ac7d9f3
Daniel Rodrigues da Silva <daniels@take.net>
2019-10-22 09:11:47 -0300

    NEW: Add option to delete resource

--- index.js
+++ index.js
@@ -25,9 +25,29 @@ server.get('/resources', async function(req, res) {
     res.json('OK');
 });
 
+server.get('/delete-resource', async function(req, res) {
+    const resources = await axios({
+        method: 'POST',
+        url: process.env.API_URL,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': process.env.SENDER
+        }, 
+        data: {  
+            "id": uuidv4(),
+            "method": "get",
+            "uri": "/resources"
+        }
+    });
+
+    deleteResource(resources);
+
+    res.json('OK');
+});
+
 function getByResource(resources) {
     resources.data.resource.items.forEach(async (element) => {
-        var test = await axios({
+        var itemResource = await axios({
             method: 'POST',
             url: process.env.API_URL,
             headers: {
@@ -40,7 +60,7 @@ function getByResource(resources) {
                 "uri": "/resources/"+element
             }
         });
-        await setResource(element, test.data.resource)
+        await setResource(element, itemResource.data.resource)
     });
 
     return true;
@@ -61,6 +81,26 @@ async function setResource(element, resource) {
             "type": "text/plain",
             "resource": resource
         }
+    });
+
+    return true;
+}
+
+function deleteResource(resources) {
+    resources.data.resource.items.forEach(async (element) => {
+        await axios({
+            method: 'POST',
+            url: process.env.API_URL,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': process.env.SENDER
+            },
+            data: {  
+                "id": uuidv4(),
+                "method": "delete",
+                "uri": "/resources/"+element
+            }
+        });
     });
 
     return true;
